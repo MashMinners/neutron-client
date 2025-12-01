484e75dbe1a52a67c41f343adb5328b15edd84b1
MashMinners <Valehtelit@mail.ru>
2025-12-01 12:04:31 +1000

    #Аватары добавлены

--- src/configs/prime.vue.modules.js
+++ src/configs/prime.vue.modules.js
@@ -21,12 +21,13 @@ import Tab from 'primevue/tab';
 import TabPanels from 'primevue/tabpanels';
 import TabPanel from 'primevue/tabpanel';
 import Drawer from "primevue/drawer";
+import Avatar from "primevue/avatar";
 import ('primeicons/primeicons.css')
 import ('primeflex/primeflex.css');
 export default {
   modules: [
       InputText, Button, Fieldset, ProgressBar, Dialog,  Message, Textarea, Image, Card, Knob, Panel, Toolbar,
-      Menu, DataTable, Column, Tabs, TabList, TabPanel, Tab, TabPanels, Drawer
+      Menu, DataTable, Column, Tabs, TabList, TabPanel, Tab, TabPanels, Drawer, Avatar
   ],
   directives: [
         {name: "tooltip", subject: Tooltip}
