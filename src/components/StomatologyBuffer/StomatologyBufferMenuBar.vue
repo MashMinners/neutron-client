fcaf66d960c79dff65a98cac16eb208c1a0ddea1
MashMinners <Valehtelit@mail.ru>
2025-12-02 16:36:21 +1000

    #Badge

--- src/components/StomatologyBuffer/StomatologyBufferMenuBar.vue
+++ src/components/StomatologyBuffer/StomatologyBufferMenuBar.vue
@@ -1,6 +1,15 @@
 <template>
 <div>
-  <prime-menubar :model="menuBarItems"></prime-menubar>
+  <prime-menubar :model="menuBarItems">
+    <template #item="{item, props, hasSubmenu, root}">
+      <a v-ripple class="flex items-center" v-bind="props.action">
+        <span>{{ item.label }}</span>
+        <prime-badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
+        <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
+        <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
+      </a>
+    </template>
+  </prime-menubar>
 </div>
 </template>
 
@@ -72,6 +81,7 @@ export default {
         },
         {
           label: 'Пересечения (30 дней)',
+          badge: 3,
           command: ()=> {
             this.setStomatologyBufferActiveComponent('StomatologyIntersections');
             //this.getIntersections();
